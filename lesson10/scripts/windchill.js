68857bfc3863e24c8d03f4fe1510e6831f13fa74
Wagner Filgueiras <38699596+wagner-filgueiras@users.noreply.github.com>
2018-06-28 18:06:18 -0300

    Update windchill.js

--- lesson10/scripts/windchill.js
+++ lesson10/scripts/windchill.js
@@ -3,5 +3,7 @@ function windchillcalc(){
 
 	var s = parseInt(document.getElementById("WindSpeed").innerHTML);
 
-document.getElementById("Windchill").innerHTML = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16));
+document.getElementById("Windchill1").innerHTML = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16));
+document.getElementById("Windchill2").innerHTML = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16));
+document.getElementById("Windchill3").innerHTML = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16));
 }
