ea9ddee23e59ebfb32868eff942757d22e29031c
Wagner Filgueiras <38699596+wagner-filgueiras@users.noreply.github.com>
2018-06-30 23:07:27 -0300

    Update windchill.js

--- lesson10/scripts/windchill.js
+++ lesson10/scripts/windchill.js
@@ -7,5 +7,34 @@ function franklinWindchill(){
 
 
 }
+
 document.getElementById("Franklin-Windchill").innerHTML = franklinWindchill();
+
+function greenvilleWindchill(){
+    var t = (parseInt(document.getElementById("Greenville-High").innerHTML) + parseInt(document.getElementById("Greenville-Low").innerHTML)) / 2;
+
+	var s = parseFloat(document.getElementById("Greenville-WindSpeed").innerHTML);
+
+  return (Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16)));
+
+  
+}
+
+document.getElementById("Greenville-Windchill").innerHTML = franklinWindchill();
+
+function springfieldWindchill(){
+    var t = (parseInt(document.getElementById("Springfield-High").innerHTML) + parseInt(document.getElementById("Springfield-Low").innerHTML)) / 2;
+
+	var s = parseFloat(document.getElementById("Springfield-WindSpeed").innerHTML);
+
+  return (Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16)));
+
+  
+}
+
+document.getElementById("Springfield-Windchill").innerHTML = franklinWindchill();
+
+
+
+
 
