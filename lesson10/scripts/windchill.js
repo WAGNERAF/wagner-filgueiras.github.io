3b8e3a32ea69710c8e2a7f4e84240768629ee294
Wagner Filgueiras <38699596+wagner-filgueiras@users.noreply.github.com>
2018-06-30 21:25:44 -0300

    Update windchill.js

--- lesson10/scripts/windchill.js
+++ lesson10/scripts/windchill.js
@@ -1,9 +1,11 @@
-function windchillcalc(){
-    var t = (parseInt(document.getElementById("High").innerHTML) + parseFloat(document.getElementById("Low").innerHTML)) / 2;
+function franklinWindchill(){
+    var t = (parseInt(document.getElementById("Franklin-High").innerHTML) + parseInt(document.getElementById("Franklin-Low").innerHTML)) / 2;
 
-	var s = parseInt(document.getElementById("WindSpeed").innerHTML);
+	var s = parseFloat(document.getElementById("Franklin-WindSpeed").innerHTML);
 
-document.getElementById("Windchill1").innerHTML = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16));
-document.getElementById("Windchill2").innerHTML = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16));
-document.getElementById("Windchill3").innerHTML = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16));
+  return (Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16)));
+
+
 }
+document.getElementById("Franklin-Windchill").innerHTML = franklinWindchill();
+
