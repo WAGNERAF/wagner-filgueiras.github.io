0b1d4926121d4b43a53e2f6e45515ac583b5648c
Wagner Filgueiras <38699596+wagner-filgueiras@users.noreply.github.com>
2018-06-30 23:21:40 -0300

    Update windchill.js

--- lesson10/scripts/windchill.js
+++ lesson10/scripts/windchill.js
@@ -1,38 +1,42 @@
 function franklinWindchill(){
-    var t = (parseInt(document.getElementById("Franklin-High").innerHTML) + parseInt(document.getElementById("Franklin-Low").innerHTML)) / 2;
+    var t = (parseInt(document.getElementById("franklin-high").innerHTML) + parseInt(document.getElementById("franklin-low").innerHTML)) / 2;
 
-	var s = parseFloat(document.getElementById("Franklin-WindSpeed").innerHTML);
+	var s = parseFloat(document.getElementById("franklin-wind-speed").innerHTML);
 
   return (Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16)));
 
 
 }
 
-document.getElementById("Franklin-Windchill").innerHTML = franklinWindchill();
+document.getElementById("franklin-windchill").innerHTML = franklinWindchill();
+
+
 
 function greenvilleWindchill(){
-    var t = (parseInt(document.getElementById("Greenville-High").innerHTML) + parseInt(document.getElementById("Greenville-Low").innerHTML)) / 2;
+    var t = (parseInt(document.getElementById("greenville-high").innerHTML) + parseInt(document.getElementById("greenville-low").innerHTML)) / 2;
 
-	var s = parseFloat(document.getElementById("Greenville-WindSpeed").innerHTML);
+	var s = parseFloat(document.getElementById("greenville-wind-speed").innerHTML);
 
   return (Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16)));
 
   
 }
 
-document.getElementById("Greenville-Windchill").innerHTML = franklinWindchill();
+document.getElementById("greenville-windchill").innerHTML = franklinWindchill();
+
+
 
 function springfieldWindchill(){
-    var t = (parseInt(document.getElementById("Springfield-High").innerHTML) + parseInt(document.getElementById("Springfield-Low").innerHTML)) / 2;
+    var t = (parseInt(document.getElementById("springfield-high").innerHTML) + parseInt(document.getElementById("springfield-low").innerHTML)) / 2;
 
-	var s = parseFloat(document.getElementById("Springfield-WindSpeed").innerHTML);
+	var s = parseFloat(document.getElementById("springfield-win-speed").innerHTML);
 
   return (Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16)));
 
   
 }
 
-document.getElementById("Springfield-Windchill").innerHTML = franklinWindchill();
+document.getElementById("springfield-Windchill").innerHTML = franklinWindchill();
 
 
 
