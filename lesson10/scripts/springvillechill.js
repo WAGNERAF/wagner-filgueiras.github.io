9aa3929dc43fa91badd7dcd5c67f1a2f02c6c4cc
Wagner Filgueiras <38699596+wagner-filgueiras@users.noreply.github.com>
2018-07-03 01:28:28 -0300

    Create springvillechill.js

---
+++ lesson10/scripts/springvillechill.js
@@ -0,0 +1,11 @@
+function springfieldWindchill(){
+    var t = (parseInt(document.getElementById("springfield-high").innerHTML) + parseInt(document.getElementById("springfield-low").innerHTML)) / 2;
+
+	var s = parseFloat(document.getElementById("springfield-win-speed").innerHTML);
+
+  return (Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16)));
+
+  
+}
+
+document.getElementById("springfield-windchill").innerHTML = springfieldWindchill();
