35642a036e777eaba75645475f6201ddc9b26eb0
Wagner Filgueiras <38699596+wagner-filgueiras@users.noreply.github.com>
2018-07-10 15:34:22 -0300

    Create franklinwindchill.js

---
+++ lesson6/scripts/franklinwindchill.js
@@ -0,0 +1,12 @@
+function franklinWindchill(){
+    var t = (parseInt(document.getElementById("franklin-high").innerHTML) + parseInt(document.getElementById("franklin-low").innerHTML)) / 2;
+
+	var s = parseFloat(document.getElementById("franklin-wind-speed").innerHTML);
+
+  return (Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s,0.16)));
+
+  
+  
+}
+
+document.getElementById("franklin-windchill").innerHTML = franklinWindchill();
