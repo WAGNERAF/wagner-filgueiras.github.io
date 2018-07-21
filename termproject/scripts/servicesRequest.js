7256cd3808d8a379ee962a7d2ff502edff60b56a
Wagner Filgueiras <38699596+wagner-filgueiras@users.noreply.github.com>
2018-07-20 23:30:24 -0300

    Update servicesRequest.js

--- termproject/scripts/servicesRequest.js
+++ termproject/scripts/servicesRequest.js
@@ -4,7 +4,8 @@ serviceRequest.send();
 serviceRequest.onload = function () {
     var services = JSON.parse(serviceRequest.responseText);
     console.log(services);
-
+    
+    // Springfield
     document.getElementById("first_service").innerHTML = services.services[0].name;
     document.getElementById("first_price").innerHTML = services.services[0].price;
     document.getElementById("first_time").innerHTML = services.services[0].timeEstimated;
@@ -12,17 +13,23 @@ serviceRequest.onload = function () {
 
 
     // Springfield
-    document.getElementById("springfield-founded").innerHTML = towns.towns[1].yearFounded;
-    document.getElementById("springfield-motto").innerHTML = towns.towns[1].motto;
-    document.getElementById("springfield-population").innerHTML = towns.towns[1].currentPopulation;
-    document.getElementById("springfield-rainfall").innerHTML = towns.towns[1].averageRainfall;
+    document.getElementById("second_service").innerHTML = services.services[0].name;
+    document.getElementById("second_price").innerHTML = services.services[0].price;
+    document.getElementById("second_time").innerHTML = services.services[0].timeEstimated;
+    document.getElementById("second_detail").innerHTML = services.services[0].details;
 
 
     //Greenville
-    document.getElementById("greenville-founded").innerHTML = towns.towns[1].yearFounded;
-    document.getElementById("greenville-motto").innerHTML = towns.towns[1].motto;
-    document.getElementById("greenville-population").innerHTML = towns.towns[1].currentPopulation;
-    document.getElementById("greenville-rainfall").innerHTML = towns.towns[1].averageRainfall;
+    document.getElementById("third_service").innerHTML = services.services[0].name;
+    document.getElementById("third_price").innerHTML = services.services[0].price;
+    document.getElementById("third_time").innerHTML = services.services[0].timeEstimated;
+    document.getElementById("third_detail").innerHTML = services.services[0].details;
+    
+     //Greenville
+    document.getElementById("fourth_service").innerHTML = services.services[0].name;
+    document.getElementById("fourth_price").innerHTML = services.services[0].price;
+    document.getElementById("fourth_time").innerHTML = services.services[0].timeEstimated;
+    document.getElementById("fourth_detail").innerHTML = services.services[0].details;
 
 
 };
